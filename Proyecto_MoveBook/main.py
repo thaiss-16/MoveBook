33b7030b7f42e60b453b1e05c1cb1facd099d814
Thais González <thais.gonzalez2009@gmail.com>
2026-07-25 12:52:06 -0400

    Refactor Flask app to use base directory for paths

--- Proyecto_MoveBook/main.py
+++ Proyecto_MoveBook/main.py
@@ -2,7 +2,9 @@ from flask import Flask, session, redirect, url_for, render_template, request, j
 import os
 import sqlite3
 
-app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'), static_folder=os.path.join(os.getcwd(), 'assets'))
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder=os.path.join(base_dir, 'templates'), static_folder=os.path.join(base_dir, 'assets'))
 app.secret_key = 'clave_secreta_super_segura_>:)'
 
 from db import db
